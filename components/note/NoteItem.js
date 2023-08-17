1a2f35edf84a2e68857fc121e87fab3129ee249d
daoistcoder <jeromeballena.dev@gmail.com>
2023-08-18 01:14:53 +0800

    UPDATE: ui

--- components/note/NoteItem.js
+++ components/note/NoteItem.js
@@ -1,6 +1,7 @@
 import styles from '../../styles/Todo.module.css'
-import { CalendarIcon, TrashIcon, PencilAltIcon } from '@heroicons/react/outline'
+import { CalendarIcon, TrashIcon, PencilAltIcon, CheckCircleIcon } from '@heroicons/react/outline'
 
+import React from 'react'
 const NoteItem = ({ idx, content, marked, dateline, publicKey, action }) => {
     const handleMarkTodo = () => {
         // Only allow unchecked todo to be marked
@@ -18,23 +19,50 @@ const NoteItem = ({ idx, content, marked, dateline, publicKey, action }) => {
 
     }
 
+    const [updateContent, setUpdateContent] = React.useState([])
+    const isEditing = true;
+    const handleChange = (e)=> {
+        setUpdateContent(e.target.value)
+    }
     return (
-        <li key={idx} className={styles.todoItem}>
-            <div onClick={handleMarkTodo} className={`${styles.todoCheckbox} ${marked && styles.checked}`} />
-            <div>
-                <span className="todoText">{content}</span>
-                {dateline && (
-                    <div className={styles.todoDateline}>
-                        <CalendarIcon className={styles.calendarIcon} />
-                        <span>{dateline}</span>
+        <>
+            {isEditing ? (
+                <li key={idx} className={styles.todoItem}>
+                    <div className={styles.todoCheckbox} />
+                    <div>
+                        <input 
+                            id = {styles.noteInputField}
+                            type="text"
+                            onChange={handleChange}
+                            value= {updateContent}
+                            placeholder= {content}
+                        >
+                        </input>
                     </div>
-                )}
-            </div>
-            <div className={styles.iconContainer}>
-                <PencilAltIcon className={styles.pencilAltIcon} />
-                <TrashIcon onClick={handleRemoveTodo} className={`${styles.trashIcon} ${!marked && styles.checked}`} />
-            </div>
-        </li>
+                    <div className={styles.iconContainer}>
+                        <CheckCircleIcon className={styles.pencilAltIcon} />
+                    </div>
+                </li>
+            ) : (
+                <li key={idx} className={styles.todoItem}>
+                    <div onClick={handleMarkTodo} className={`${styles.todoCheckbox} ${marked && styles.checked}`} />
+                    <div>
+                        <span className="todoText">{content}</span>
+                        {dateline && (
+                            <div className={styles.todoDateline}>
+                                <CalendarIcon className={styles.calendarIcon} />
+                                <span>{dateline}</span>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.iconContainer}>
+                        <PencilAltIcon className={styles.pencilAltIcon} />
+                        <TrashIcon onClick={handleRemoveTodo} className={`${styles.trashIcon} ${!marked && styles.checked}`} />
+                    </div>
+                </li>
+            )}
+        </>
+
     )
 }
 
